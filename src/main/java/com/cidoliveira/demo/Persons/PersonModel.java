f3fce36bfd588d70a4e3b930f5181b236f57ad93
cid <alcides.angelo10@gmail.com>
2026-09-09 15:56:14 -0300

    feat: using lombok for data getters/setters and constructors

--- src/main/java/com/cidoliveira/demo/Persons/PersonModel.java
+++ src/main/java/com/cidoliveira/demo/Persons/PersonModel.java
@@ -2,11 +2,17 @@ package com.cidoliveira.demo.Persons;
 
 import com.cidoliveira.demo.Tasks.TasksModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_persons")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PersonModel {
 
     @Id
@@ -20,37 +26,4 @@ public class PersonModel {
     @JoinColumn(name = "tasks_id")
     private TasksModel userTasks;
 
-
-    public PersonModel() {
-    }
-
-    public PersonModel(String personName, String personEmail, int personAge) {
-        this.personName = personName;
-        this.personEmail = personEmail;
-        this.personAge = personAge;
-    }
-
-    public String getPersonName() {
-        return personName;
-    }
-
-    public void setPersonName(String personName) {
-        this.personName = personName;
-    }
-
-    public String getPersonEmail() {
-        return personEmail;
-    }
-
-    public void setPersonEmail(String personEmail) {
-        this.personEmail = personEmail;
-    }
-
-    public int getPersonAge() {
-        return personAge;
-    }
-
-    public void setPersonAge(int personAge) {
-        this.personAge = personAge;
-    }
 }
--- src/main/java/com/cidoliveira/demo/Tasks/TasksModel.java
+++ src/main/java/com/cidoliveira/demo/Tasks/TasksModel.java
@@ -2,11 +2,17 @@ package com.cidoliveira.demo.Tasks;
 
 import com.cidoliveira.demo.Persons.PersonModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_tasks")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class TasksModel {
 
     @Id
@@ -18,44 +24,4 @@ public class TasksModel {
     @OneToMany(mappedBy = "userTasks")
     private List<PersonModel> assignedPersons;
 
-    public TasksModel() {
-    }
-
-    public TasksModel(long taskId, String taskName, String taskDifficulty) {
-        this.taskId = taskId;
-        this.taskName = taskName;
-        this.taskDifficulty = taskDifficulty;
-    }
-
-    public long getTaskId() {
-        return taskId;
-    }
-
-    public void setTaskId(long taskId) {
-        this.taskId = taskId;
-    }
-
-    public String getTaskName() {
-        return taskName;
-    }
-
-    public void setTaskName(String taskName) {
-        this.taskName = taskName;
-    }
-
-    public String getTaskDifficulty() {
-        return taskDifficulty;
-    }
-
-    public void setTaskDifficulty(String taskDifficulty) {
-        this.taskDifficulty = taskDifficulty;
-    }
-
-    public PersonModel getAssignedPerson() {
-        return (PersonModel) assignedPersons;
-    }
-
-    public void setAssignedPerson(List<PersonModel> assignedPersons) {
-        this.assignedPersons = assignedPersons;
-    }
 }
