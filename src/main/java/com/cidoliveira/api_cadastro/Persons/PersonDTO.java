2645a0850a9c657ce9916d5a0e7086b82773d0bd
cid <alcides.angelo10@gmail.com>
2026-09-16 09:54:55 -0300

    chore: including DTO in person entity and manually mapping through PersonMapper the routes of entity x DTO

--- src/main/java/com/cidoliveira/api_cadastro/Persons/PersonDTO.java
+++ src/main/java/com/cidoliveira/api_cadastro/Persons/PersonDTO.java
@@ -1,4 +1,21 @@
 package com.cidoliveira.api_cadastro.Persons;
 
+import com.cidoliveira.api_cadastro.Tasks.TasksModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PersonDTO {
+
+    private Long personId;
+    private String personName;
+    private String personEmail;
+    private int personAge;
+    private TasksModel userTasks;
+    private String personProfession;
+
 }
--- src/main/java/com/cidoliveira/api_cadastro/Persons/PersonMapper.java
+++ src/main/java/com/cidoliveira/api_cadastro/Persons/PersonMapper.java
@@ -1,4 +1,33 @@
 package com.cidoliveira.api_cadastro.Persons;
 
+import com.cidoliveira.api_cadastro.Tasks.TasksModel;
+import jakarta.persistence.*;
+import org.springframework.stereotype.Component;
+
+@Component
 public class PersonMapper {
+
+    public PersonModel map(PersonDTO personDTO) {
+        PersonModel personModel = new PersonModel();
+        personModel.setPersonId(personDTO.getPersonId());
+        personModel.setPersonAge(personDTO.getPersonAge());
+        personModel.setPersonEmail(personDTO.getPersonEmail());
+        personModel.setPersonName(personDTO.getPersonName());
+        personModel.setPersonProfession(personDTO.getPersonProfession());
+        personModel.setUserTasks(personDTO.getUserTasks());
+
+        return personModel;
+    }
+
+    public PersonDTO map(PersonModel personModel) {
+        PersonDTO personDTO = new PersonDTO();
+        personDTO.setPersonId(personModel.getPersonId());
+        personDTO.setPersonAge(personModel.getPersonAge());
+        personDTO.setPersonEmail(personModel.getPersonEmail());
+        personDTO.setPersonName(personModel.getPersonName());
+        personDTO.setPersonProfession(personModel.getPersonProfession());
+        personDTO.setUserTasks(personModel.getUserTasks());
+
+        return personDTO;
+    }
 }
