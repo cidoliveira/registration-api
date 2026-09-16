0afd29a6da9ff9b1aa3deb9ecf7c5864b382e735
cid <alcides.angelo10@gmail.com>
2026-09-16 16:50:55 -0300

    feat: creating mapper & DTO for tasks entity

---
+++ src/main/java/com/cidoliveira/api_cadastro/Tasks/TasksDTO.java
@@ -0,0 +1,18 @@
+package com.cidoliveira.api_cadastro.Tasks;
+
+import com.cidoliveira.api_cadastro.Persons.PersonModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TasksDTO {
+    private long taskId;
+    private String taskName;
+    private String taskDifficulty;
+    private List<PersonModel> assignedPersons;
+}
